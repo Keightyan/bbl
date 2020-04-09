73e270b65526c83fc743688a580ea004d768a89c
Keightyan <keightyan@gmail.com>
2020-04-09 14:15:56 +0900

    LaravelCollectiveのインストール、ユーザー登録フォームの設置と登録の仕組みづくり

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,66 @@
+<header>
+    	<div id="header_wrap">
+    		<section id="info">
+    			<span>野球人のためのレクチャー共有サイト</span>
+    		</section>
+
+    		<section id="sns">
+    			<ul>
+    				<li><img src="https://amanoshokudo.jp/common/images/icon/i_fb.png"></li>
+    				<li><img src="https://amanoshokudo.jp/common/images/icon/i_twi.png"></li>
+    			</ul>
+    		</section>
+    	</div>
+    </header>
+
+
+    <section id="wrapper">
+
+    <section id="header2">
+    	<section id="title">
+    		<div id="logo">
+          <a href="/"></a>
+    			<h1>BaseBall Library</h1>
+    			<p>野球好きが集う知恵袋</p>
+    		</div>
+    	</section>
+
+    <section id="ad">
+    	広告
+    </section>
+
+    		<ul>
+            <li>{!! link_to_route('signup.get', 'ユーザー登録', [], []) !!}</li>
+    		<li><a href="/about">BBLについて</a></li>
+    		<li><a href="">いいい</a></li>
+    		<li><a href="">ううう</a></li>
+    		</ul>
+    </section>
+
+    	<div class="clear"></div>
+
+      <section id="sp_menu">
+        スマホ用メニュー
+      </section>
+
+      <section id="sp_info">
+<p>「<span style="color: red;"> ●● </span>を<span style="font-size: 22px;">意識</span>して練習してます」、
+「<span style="color: red;"> ●● </span>を<span style="color: red;"> ●● </span>したら<span style="font-size: 22px;">改善</span>したよ」等、
+<p style="text-align: center;">あなたの <span style="color: #0a4974; font-size: 24px; text-decoration: underline; font-weight: bold;">野球ノウハウ</span> をお待ちしています！</p>
+</section>
+
+      <!-- 各ページで適宜、id名やclass名を変えて対応する
+      <section id="sp_info">
+      <p>「<span style="color: red;"> ●● </span>を<span style="font-size: 22px;">意識</span>して練習してます」、
+      「<span style="color: red;"> ●● </span>を<span style="color: red;"> ●● </span>したら<span style="font-size: 22px;">改善</span>したよ」等、
+      <p style="text-align: center;">あなたの <span style="color: #0a4974; font-size: 24px; text-decoration: underline; font-weight: bold;">野球ノウハウ</span> をお待ちしています！</p>
+      </section>
+      -->
+
+    <section id="catch">
+  <div>
+  <p>「<span style="color: red;"> ●● </span>を<span style="font-size: 22px;">意識</span>して練習してます」、
+  「<span style="color: red;"> ●● </span>を<span style="color: red;"> ●● </span>したら<span style="font-size: 22px;">改善</span>したよ」等、
+  <p style="text-align: center;">あなたの <span style="color: #0a4974; font-size: 24px; text-decoration: underline; font-weight: bold;">野球ノウハウ</span> をお待ちしています！</p>
+  </div>
+</section>
